3dc25f1107346bdf23c556f9dba5448040a942f8
bbullemer <barbara.bullemer@gmail.com>
2019-08-21 15:04:11 -0500

    CI Cust Search
    - took out div that was creating issue with pagination
    - corrected parameter for CustomerIndex class call to also fix pagination, wrong div was loading into #cust-index div

--- site/templates/content/customer/ajax/load/cust-index/search-form.php
+++ site/templates/content/customer/ajax/load/cust-index/search-form.php
@@ -2,7 +2,7 @@
 	use Dplus\Dpluso\Customer\CustomerIndex;
 	$url = new Purl\Url($page->fullURL->getUrl());
 	$url->path = $config->pages->ajax."load/customers/cust-index/";
-	$custindex = new CustomerIndex($url, '#cust-index', '#cust-index');
+	$custindex = new CustomerIndex($url, '#cust-index2', '#cust-index2');
 	$states = $custindex->get_statesbylogin();
 ?>
 <div id="cust-index2">
@@ -22,14 +22,14 @@
 			</div>
 		</div>
 		<div class="row mb-5">
-				<div class="col-sm-12">
-					<button class="btn btn-primary toggle-order-search pull-right" type="button" data-toggle="collapse" data-target="#cust-search-div" aria-expanded="false" aria-controls="cust-search-div">Toggle Filter Search <i class="fa fa-search" aria-hidden="true"></i></button>
-				</div>
+			<div class="col-sm-12">
+				<button class="btn btn-primary toggle-order-search pull-right" type="button" data-toggle="collapse" data-target="#cust-search-div" aria-expanded="false" aria-controls="cust-search-div">Toggle Filter Search <i class="fa fa-search" aria-hidden="true"></i></button>
 			</div>
+		</div>
 
-			<div id="cust-search-div" class="collapse">
-				<?php include $config->paths->content."customer/ajax/load/cust-index/filter-search-form.php"; ?>
-			</div>
+		<div id="cust-search-div" class="collapse">
+			<?php include $config->paths->content."customer/ajax/load/cust-index/filter-search-form.php"; ?>
+		</div>
 	</form>
 	<div>
 		<?php
